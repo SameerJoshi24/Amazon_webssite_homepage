d9054793a2cff9b77845aad0823a4c2c9434d37b
SameerJoshi24 <joshisameer631@mail.com>
2023-05-01 09:57:09 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Coupon code 
+function loadcoupon () {
+    document.getElementsById('couponimg').style = 'visible';
+    document.getElementsByTagName("body").style.opacity = '0.7'
+
+} 
+function closecoupon () {
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementsByTagName("body").style.opacity = '1'
+}
+// Dark-white mode 
 function changeMode(){
     let mybody = document.body
     mybody.classList.toggle('darkmode')
